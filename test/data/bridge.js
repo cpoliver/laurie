ccd683999e135128ca2b448fbebab6e3174ff297
Charles Oliver <cpoliver@gmail.com>
2016-09-22 09:13:01 +0100

    added bridge test data response

---
+++ test/data/bridge.js
@@ -0,0 +1,48 @@
+module.exports = {
+  name: 'Philips hue',
+    zigbeechannel: 11,
+    mac: 'xx:xx:xx:xx:xx:xx',
+    dhcp: false,
+    ipaddress: '192.168.2.245',
+    netmask: '255.255.255.0',
+    gateway: '192.168.2.254',
+    proxyaddress: 'none',
+    proxyport: 0,
+    UTC: '2015-01-10T13:18:51',
+    localtime: '2015-01-10T13:18:51',
+    timezone: 'Europe/London',
+    whitelist: {
+      fG2EZIaS2pZuSeKH: {
+        'last use date': '2015-01-09T22:54:21',
+        'create date': '2014-05-18T17:11:10',
+        name: 'philips.lighting.hue#iPad'
+      },
+      '0f607264fc6318a92b9e13c65db7cd3c': {
+        'last use date': '2014-12-23T17:25:16',
+        'create date': '2014-12-23T17:14:30',
+        name: 'iPad'
+      }
+    },
+    swversion: '01018228',
+    apiversion: '1.5.0',
+    swupdate: {
+      updatestate: 0,
+      checkforupdate: false,
+      devicetypes: {
+        bridge: false,
+        lights: []
+      },
+      url: '',
+      text: '',
+      notify: false
+    },
+    linkbutton: false,
+    portalservices: true,
+    portalconnection: 'connected',
+    portalstate: {
+      signedon: true,
+      incoming: true,
+      outgoing: true,
+      communication: 'connected'
+    }
+};
