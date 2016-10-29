0c1b953b62f735c8d1eda6c025970453a46a4b6e
Charles Oliver <cpoliver@gmail.com>
2016-10-29 17:34:21 +0100

    removed unused (copy/pasted) contents of reducers/index

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,1 @@
-import React from 'react';
-import { render } from 'react-dom';
 
-import './styles.css';
-
-import App from './components/app';
-
-render(
-  <App/>,
-  document.getElementById('root')
-);
